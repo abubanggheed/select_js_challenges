77bbe8b486bc5175e251429145bc3e0b1fe30219
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-01-17 02:15:55 -0600

    permdas fixed to handle negative numbers

--- permdas.js
+++ permdas.js
@@ -5,6 +5,16 @@ multiplications, divisions, additions, and subtractions and returns the
 numerical solution to that equation.
 */
 
+const formatExpression = expression => (
+    expression.split('').map((char, index) => (
+        char === '-' ? (
+            (index === 0 || '+-*/^('.indexOf(expression[index - 1]) > -1) ?
+                '-' : '+-'
+        ) : char
+    )
+    ).join('')
+)
+
 const findClossingP = (expression, startingIndex) => {
     let i = startingIndex + 1;
     let count = 1;
@@ -24,28 +34,24 @@ const findClossingP = (expression, startingIndex) => {
 const permdasHelper = expression => {
     let clossingP = 0;
     let openingP = expression.lastIndexOf('(');
-    let lastPlus = (openingP === -1) ? expression.lastIndexOf('+'): 1;
-    let lastMinus = (openingP === -1) ? expression.lastIndexOf('-'): 1;
-    let lastTimes = (lastPlus + lastMinus === -2) ? expression.lastIndexOf('*'): 1;
-    let lastDiv = (lastPlus + lastMinus === -2) ? expression.lastIndexOf('/'): 1;
-    let lastExp = (lastDiv + lastTimes === -2) ? expression.lastIndexOf('^'): 1;
+    let lastPlus = (openingP === -1) ? expression.lastIndexOf('+') : 1;
+    let lastTimes = (lastPlus === -1) ? expression.lastIndexOf('*') : 1;
+    let lastDiv = (lastPlus === -1) ? expression.lastIndexOf('/') : 1;
+    let lastExp = (lastDiv + lastTimes === -2) ? expression.lastIndexOf('^') : 1;
     if (openingP > -1) {
         clossingP = findClossingP(expression, openingP)
         return permdasHelper(expression.slice(0, openingP)
             + permdasHelper(expression.slice(openingP + 1, clossingP))
             + expression.substr(clossingP + 1))
-    } else if (lastPlus > -1 || lastMinus > -1) {
-        if (lastPlus > lastMinus) {
-            return permdasHelper(expression.slice(0, lastPlus)) + permdasHelper(expression.substr(lastPlus + 1))
-        } else {
-            return permdasHelper(expression.slice(0, lastMinus)) - permdasHelper(expression.substr(lastMinus + 1))
-        }
+    } else if (lastPlus > -1) {
+        return permdasHelper(expression.slice(0, lastPlus)) + permdasHelper(expression.substr(lastPlus + 1))
     } else if (lastTimes > -1 || lastDiv > -1) {
         if (lastTimes > lastDiv) {
             return permdasHelper(expression.slice(0, lastTimes)) * permdasHelper(expression.substr(lastTimes + 1))
         } else {
             return permdasHelper(expression.slice(0, lastDiv)) / permdasHelper(expression.substr(lastDiv + 1))
-        }    } else if (lastExp > -1) {
+        }
+    } else if (lastExp > -1) {
         return Math.pow(permdasHelper(expression.slice(0, lastExp)), permdasHelper(expression.substr(lastExp + 1)))
     } else {
         return Number(expression)
@@ -53,5 +59,5 @@ const permdasHelper = expression => {
 }
 
 const permdas = expression => (
-    permdasHelper(expression.replace(/ /g, ''))
+    permdasHelper(formatExpression(expression))
 )
