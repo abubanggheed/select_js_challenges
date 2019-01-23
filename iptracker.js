e9193443f8aa35077704e6963d281d20f2720432
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-01-23 14:48:32 -0600

    made constructors for all classes to use

---
+++ iptracker.js
@@ -0,0 +1,29 @@
+class IpInstance {
+  constructor(timestamp, parent=null, child=null) {
+    this.address = address;
+    this.timestamp = timestamp;
+    this.parent = parent;
+    this.child = child;
+  }
+}
+
+class IpBucketlist {
+  constructor(address, maxLength, timespan, parent=null, length=0, leftChild=null, rightChild=null, root=null, tail=null) {
+    this.address = address;
+    this.maxLength = maxLength;
+    this.timespan = timespan;
+    this.parent = parent;
+    this.length = length;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
+    this.root = root;
+    this.tail = tail;
+  }
+}
+
+class IpTracker {
+  constructor(maxDepth, root=null) {
+    this.maxDepth = maxDepth;
+    this.root = root;
+  }
+}
