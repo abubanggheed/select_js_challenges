d074c5840013109a52b5c5b6aeae49e5e58f6d44
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-01-24 14:02:09 -0600

    added appending method to buckets

--- iptracker.js
+++ iptracker.js
@@ -21,7 +21,7 @@ class IpBucketlist {
     this.tail = tail;
   }
 
-  removeOldRoot(time) {
+  removeOldRoot(timeThreshold) {
     let instance = this.root;
     if (this.root && instance.timestamp < time) {
       this.root = this.root.next;
@@ -31,9 +31,26 @@ class IpBucketlist {
       } else {
         this.tail = null;
       }
-      return removeOldRoot(time);
+      return removeOldRoot(timeThreshold);
     } else {
       return this.length
+    }
+  }
+
+  appendToTail() {
+    let currentTime = Date.now();
+    let instance = new IpInstance(currentTime);
+    if (this.tail) {
+      this.tail.next = instance;
+      instance.prev = this.tail;
+      let timeThreshold = currentTime - timespan;
+      this.tail = instance;
+      if (this.removeOldRoot(timeThreshold) > this.maxLength) {
+        return true;
+      }
+    } else {
+      this.tail = instance;
+      this.root = instance;
     }
   }
 }
