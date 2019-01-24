ff09c10da73ac3a478a29ce48b6d8dae4b59c78e
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-01-24 13:51:33 -0600

    changed attribute names and added remove root method

--- iptracker.js
+++ iptracker.js
@@ -1,14 +1,14 @@
 class IpInstance {
-  constructor(timestamp, parent=null, child=null) {
+  constructor(timestamp, prev = null, next = null) {
     this.address = address;
     this.timestamp = timestamp;
-    this.parent = parent;
-    this.child = child;
+    this.prev = prev;
+    this.next = next;
   }
 }
 
 class IpBucketlist {
-  constructor(address, date, maxLength, timespan, parent=null, length=0, leftChild=null, rightChild=null, root=null, tail=null) {
+  constructor(address, date, maxLength, timespan, parent = null, length = 0, leftChild = null, rightChild = null, root = null, tail = null) {
     this.address = address;
     this.date = date;
     this.maxLength = maxLength;
@@ -20,11 +20,29 @@ class IpBucketlist {
     this.root = root;
     this.tail = tail;
   }
+
+  removeOldRoot(time) {
+    let instance = this.root;
+    if (this.root && instance.timestamp < time) {
+      this.root = this.root.next;
+      this.length --;
+      if (this.root !== null) {
+        this.root.prev = null
+      } else {
+        this.tail = null;
+      }
+      return removeOldRoot(time);
+    } else {
+      return this.length
+    }
+  }
 }
 
 class IpTracker {
-  constructor(maxDepth, root=null) {
+  constructor(maxDepth, maxRequests, timespan, root = null) {
     this.maxDepth = maxDepth;
     this.root = root;
+    this.maxRequests = maxRequests;
+    this.timespan = timespan;
   }
 }
