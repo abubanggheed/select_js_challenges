fa5858b5cf016b486c634611209353c3df02bd42
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-05-13 07:50:30 -0400

    fixed name of productofarray

--- productOfArray.js
+++ productOfArray.js
@@ -3,7 +3,7 @@ function productOfArray(arrayIn) {
   let a = arrayIn[0] || 0
   let i = 1
   let product = 0
-  while(a == a) {
+  while(a == a) { // see onelineisnan.js
     product = a
     a = a * arrayIn[i]
     i++
