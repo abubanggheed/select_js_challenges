f61cd04b699ef22ee4025571ce5536e9c583b1e0
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-05-13 19:10:35 -0400

    removed hint for productofarray

--- productOfArray.js
+++ productOfArray.js
@@ -3,7 +3,7 @@ function productOfArray(arrayIn) {
   let a = arrayIn[0] || 0
   let i = 1
   let product = 0
-  while(a == a) { // see onelineisnan.js
+  while(a == a) {
     product = a
     a = a * arrayIn[i]
     i++
