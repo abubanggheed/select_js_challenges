ca12ebe6fc2164738691c82949f8099607ac5c8c
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-06-26 17:52:52 -0400

    productinarray always returning a number for any array

--- productinarray.js
+++ productinarray.js
@@ -1,11 +1,11 @@
 /*
 This function takes an array and a start index and will return
-the product of all numbers starting from that index untill
-it reaches the end of the array or a non number.
+the product of all numbers starting from that index until
+it reaches the end of the array or a non number (allowing type coersion).
 If there are no numbers to multiply, it returns 0.
 */
 function productInArray(arrayIn, startIndex = 0) {
-  let a = arrayIn[startIndex] || 0
+  let a = arrayIn[startIndex] * 1 || 0
   let product = 0
   for(let i = startIndex + 1; a === a; i++) {
     product = a
