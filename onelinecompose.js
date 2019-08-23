83a9eb7559c76dcdf1ac61e4117f0a565cff1199
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-08-23 18:27:21 -0400

    simplified composeAll

--- onelinecompose.js
+++ onelinecompose.js
@@ -3,5 +3,5 @@ const compose = (f, g) => (...args) => f(g(...args))
 
 const composeAll = (...args) => args.reduceRight(
   (g, f) => g ? (...yargs) => f(g(...yargs))
-  : (...yargs) => f(...yargs)
+  : f
 )
