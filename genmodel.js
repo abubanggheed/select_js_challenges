a949f9754cbb3a9dac8d1c3e5ba3c4f278b417fa
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-09-27 14:36:05 -0400

    modified genModel to be applicable to prexisting functions and objects and switched argument order in composeAll

--- genmodel.js
+++ genmodel.js
@@ -2,11 +2,16 @@
 const genModel = (...fields) => class {
 
   constructor(...values) {
+    this.constructor.apply(this, ...values)
+  }
+
+  static apply(funcObj, ...values) {
     const capsule = [...values]
     fields.forEach((field, index) => {
       this[`get${field}`] = () => capsule[index]
       this[`set${field}`] = newValue => { capsule[index] = newValue }
-    }) 
+    })
+    return funcObj
   }
 
 }
--- onelinecompose.js
+++ onelinecompose.js
@@ -1,7 +1,7 @@
 
 const compose = (f, g) => (...args) => f(g(...args))
 
-const composeAll = (...args) => args.reduceRight(
+const composeAll = (...args) => args.reduce(
   (g, f) => g ? (...yargs) => f(g(...yargs))
   : f
 )
