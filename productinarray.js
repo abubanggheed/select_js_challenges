20a6af311fccb5e766d2fe675a687f2f6f32a190
Elias Friedman <efriedman02@gmail.com>
2020-04-22 09:33:50 -0400

    removed body of for loop in productinarray

--- productinarray.js
+++ productinarray.js
@@ -5,11 +5,11 @@ it reaches the end of the array or a non number (allowing type coersion).
 If there are no numbers to multiply, it returns 0.
 */
 function productInArray(arrayIn, startIndex = 0) {
-  let a = arrayIn[startIndex] * 1 || 0
   let product = 0
-  for(let i = startIndex + 1; a === a; i++) {
-    product = a
-    a = a * arrayIn[i]
-  }
-  return product
+  for (
+    let i = startIndex + 1, a = arrayIn[startIndex] * 1 || 0;
+    product = a, a = a * arrayIn[i], a === a;
+    i++
+  ) { }
+    return product
 }
